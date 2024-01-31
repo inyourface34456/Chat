a3256e15d5ea0479b351d01f6817d4bd0f578783
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2024-01-31 20:36:21 +0000

    did some thinsg

--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,94 @@ extern crate rocket;
 
 use rocket::form::Form;
 use rocket::fs::FileServer;
+use rocket::http::{ContentType, Status};
 use rocket::response::stream::{Event, EventStream};
+use rocket::serde::json;
 use rocket::serde::{Deserialize, Serialize};
 use rocket::tokio::select;
-use rocket::http::{ContentType, Status};
 use rocket::tokio::sync::broadcast::{channel, error::RecvError, Sender};
 use rocket::{Shutdown, State};
-use rocket::serde::json;
 use std::collections::BTreeMap;
-use std::sync::RwLock;
 use std::mem::drop;
+use std::sync::RwLock;
+use std::collections::HashMap;
 
-#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
-#[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize, Eq, Hash, PartialEq)]
+#[cfg_attr(test, derive(UriDisplayQuery))]
 #[serde(crate = "rocket::serde")]
 struct Message {
-    #[field(validate = len(..32))]
+    #[field(validate = len(2..32))]
     pub room: String,
     #[field(validate = len(..32))]
     pub username: String,
     #[field(validate = len(..65536))]
     pub message: String,
+    #[field(validate = len(..24))]
+    pub color: String,
 }
 
 struct Users {
-    pub users: RwLock<Vec<String>>
+    pub users: RwLock<Vec<String>>,
+}
+
+struct Rooms {
+    pub users: RwLock<Vec<String>>,
+}
+
+struct SavedMessages {
+    pub map: RwLock<HashMap<String, Vec<Message>>>
 }
 
 #[derive(FromForm)]
 struct UserInput {
     pub old_username: String,
-    pub new_username: String
+    pub new_username: String,
 }
 
+#[derive(FromForm)]
+struct RoomName {
+    pub room_name: String
+}
 
-fn check_repeats(s:&str)->usize {
+fn rw_lock_to_json<T: Serialize>(users: &RwLock<Vec<T>>) -> (Status, (ContentType, String)) {
+    let mut status = Status::Ok;
+
+    let json_;
+
+    loop {
+        match users.read() {
+            Ok(dat) => {
+                let mut data = vec![];
+
+                for i in dat.iter() {
+                    data.push(i);
+                }
+
+                json_ = json::to_string(&data);
+
+                drop(dat);
+                break;
+            }
+            Err(_) => continue,
+        }
+    }
+
+    let json_ = match json_ {
+        Ok(dat) => dat,
+        Err(_) => {
+            status = Status::Locked;
+            String::new()
+        }
+    };
+
+    (status, (ContentType::JSON, json_))
+}
+
+fn check_repeats(s: &str) -> usize {
     let mut delays: BTreeMap<_, std::str::Chars> = BTreeMap::new();
-    for (i,c) in s.chars().enumerate() {
-        delays.retain(|_,iter| iter.next() == Some(c));
-        delays.insert(i+1, s.chars());
+    for (i, c) in s.chars().enumerate() {
+        delays.retain(|_, iter| iter.next() == Some(c));
+        delays.insert(i + 1, s.chars());
     }
     delays.into_keys().next().unwrap()
 }
@@ -69,27 +118,56 @@ async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStrea
 
 /// Receive a message from a form submission and broadcast it to any receivers.
 #[post("/message", data = "<form>")]
-fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+fn post(form: Form<Message>, queue: &State<Sender<Message>>, rooms: &State<Rooms>, messages: &State<SavedMessages>) {
     // A send 'fails' if there are no active subscribers. That's okay.
     let inner = form.into_inner();
     let message = &inner.message;
     let username = &inner.username;
+    let room = &inner.room;
     let mut char_comp: Vec<i32> = vec![0; 256];
     let mut homogenus_comp = false;
+    let rooms = &rooms.users;
+    let messages = &messages.map;
 
     for i in message.chars() {
         char_comp[i as u8 as usize] += 1;
     }
 
     if message.len() > 50 {
-      for i in char_comp.iter() {
-          if *i > (message.len() as f32 * 0.25) as i32 {
-              homogenus_comp = true;
-          }
-      }
+        for i in char_comp.iter() {
+            if *i > (message.len() as f32 * 0.25) as i32 {
+                homogenus_comp = true;
+            }
+        }
     }
 
-    if !(homogenus_comp || check_repeats(&message.replace(' ', "")) != message.len()-message.matches(' ').count() || username == "[STATUS]" || username == "[SERVER]" || message.starts_with('/')) {
+    loop {
+        if let Ok(mut vec) = rooms.try_write() {
+            if !vec.iter().any(|e| room.contains(e)) {
+                vec.push(room.to_string());
+            }
+            break;
+        }
+    }
+
+    loop {
+        if let Ok(mut hashmap) = messages.try_write() {
+            if !hashmap.keys().any(|e| room.contains(e)) {
+                hashmap.insert(room.to_string(), vec![inner.clone()]);
+            } else {
+                hashmap.get_mut(room).unwrap().push(inner.clone());
+            }
+            break;
+        }
+    }
+
+    if !(homogenus_comp
+        || check_repeats(&message.replace(' ', "")) != message.len() - message.matches(' ').count()
+        || username == "[STATUS]"
+        || username == "[SERVER]"
+        || message.starts_with('/')
+        || username == "System")
+    {
         let _ = queue.send(inner);
     }
 }
@@ -102,69 +180,78 @@ fn add_user(total_users: &State<Users>, user_input: Form<UserInput>) {
     let users = &total_users.users;
 
     if old_username == new_username {
-      return;
+        return;
     }
-  
+
     loop {
-      if let Ok(mut vec) = users.try_write() {
-          if vec.iter().any(|e| old_username.contains(e)) {
-              match vec.iter().position(|r| r == &old_username) {
-                Some(index) => vec.remove(index),
-                None => return,
-              };
-              vec.push(new_username);
-          } else {
-              vec.push(new_username);
-          }
-        break;
-      } else {
-        continue;
-      }
+        if let Ok(mut vec) = users.try_write() {
+            if vec.iter().any(|e| old_username.contains(e)) {
+                match vec.iter().position(|r| r == &old_username) {
+                    Some(index) => vec.remove(index),
+                    None => return,
+                };
+                vec.push(new_username);
+            } else {
+                vec.push(new_username);
+            }
+            break;
+        } else {
+            continue;
+        }
     }
 }
 
 #[get("/get_users")]
 fn get_users(total_users: &State<Users>) -> (Status, (ContentType, String)) {
     let users = &total_users.users;
-    let mut status = Status::Accepted;
 
-    let json_;
-
-    loop {
-      match users.read() {
-        Ok(dat) => {
-          let mut data = vec![];
-
-          for i in dat.iter() {
-            data.push(i);
-          }
-
-          json_ = json::to_string(&data);
-
-          drop(dat);
-          break;
-        },
-        Err(_) => continue,
-      }
-    }
-
-    let json_ = match json_ {
-      Ok(dat) => dat,
-      Err(_) => {
-        status = Status::Locked;
-        String::new()
-      }
-    };
-
-    (status, (ContentType::JSON, json_))
+    rw_lock_to_json(users)
 }
 
+#[get("/get_rooms")]
+fn get_rooms(rooms: &State<Rooms>) -> (Status, (ContentType, String)) {
+    let rooms = &rooms.users;
+
+    rw_lock_to_json(rooms)
+}
+
+#[post("/messages", data = "<room_name>")]
+fn get_messages(messages: &State<SavedMessages>, room_name: Form<RoomName>) -> (Status, (ContentType, String)) {
+    let inner = room_name.into_inner();
+    let room = inner.room_name;
+    let map = &messages.map;
+    let mut messages = vec![];
+    
+    loop {
+        if let Ok(map) = map.read() {
+            if let Some(vec) = map.get(&room) {
+                for i in vec {
+                    messages.push(i.clone());
+                }
+                break;
+            } else {
+                return (Status::NotFound, (ContentType::JSON, String::new()))
+            }
+        }
+    }
+
+    rw_lock_to_json(&RwLock::new(messages))
+}
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .manage(channel::<Message>(1024).0)
-        .manage(Users {users: RwLock::new(vec![])})
-        .mount("/", routes![post, events, add_user, get_users])
+        .manage(Users {
+            users: RwLock::new(vec![]),
+        })
+        .manage(Rooms {
+            users: RwLock::new(vec!["lobby".to_string()]),
+        })
+        .manage(SavedMessages {
+            map: RwLock::new(HashMap::new())
+        }
+        )
+        .mount("/", routes![post, events, add_user, get_users, get_rooms, get_messages])
         .mount("/", FileServer::from("static"))
 }
--- static/script.js
+++ static/script.js
@@ -3,6 +3,7 @@ let messagesDiv = document.getElementById('messages');
 let newMessageForm = document.getElementById('new-message');
 let newRoomForm = document.getElementById('new-room');
 let statusDiv = document.getElementById('status');
+let refershButton = document.getElementById('refersh');
 
 let roomTemplate = document.getElementById('room');
 let messageTemplate = document.getElementById('message');
@@ -14,12 +15,62 @@ let id = 0;
 let old_username = "";
 let debug = false;
 let muted = [];
+var color = "hash";
 
 var STATE = {
 	room: "lobby",
 	rooms: {},
 	connected: false,
 };
+
+function dedupRooms() {
+	let nodes = document.getElementsByClassName("room");
+	let contained = [];
+
+	for (let i = 0; i < nodes.length; i++) {
+		if (contained.includes(nodes[i].textContent)) {
+			nodes[i].remove();
+		} else {
+			contained.push(nodes[i].textContent);
+		}
+	}
+}
+
+function refershRooms() {
+	let nodes = document.getElementsByClassName("room");
+	changeRoom("lobby");
+
+	STATE = {
+		room: "lobby",
+		rooms: {},
+		connected: STATE.connected,
+	};
+
+	for (let i = 0; i < nodes.length; i++) {
+		nodes[i].remove();
+	}
+
+	fetch("/get_rooms", {
+		method: "GET",
+	})
+	.then((response) => {
+		if(response.ok) {
+			return response.json();
+		} else {
+			addMessage(STATE.room, "[STATUS]", `failed to fetch rooms`);
+			return response.json();
+		}
+	})
+	.then((data) => {
+		for (let i = 0; i < data.length; i++) {
+			if (debug) {
+				addMessage(STATE.room, "[DEBUG]", `${addRoom(data[i], false)}`);
+			} else {
+				addRoom(data[i], false)
+			}
+		}
+	});
+}
 
 // Generate a color from a "hash" of a string. Thanks, internet.
 function hashColor(str) {
@@ -39,11 +90,31 @@ function hashColor(str) {
 
 // Add a new room `name` and change to it. Returns `true` if the room didn't
 // already exist and false otherwise.
-function addRoom(name) {
-	if(STATE[name]) {
+function addRoom(name, change=true) {
+	if(STATE[name] && change) {
 		changeRoom(name);
 		return false;
 	}
+
+	if (name.length == 1) return;
+
+	// fetch("/message", {
+	// 	method: "POST",
+	// 	body: new URLSearchParams({
+	// 		room: name,
+	// 		username: "System",
+	// 		message: `Welcome to ${name}`,
+	// 		color
+	// 	}),
+	// }).then((response) => {
+	// 	if(response.ok) {
+	// 		messageField.value = "";
+	// 	} else {
+	// 		addMessage(STATE.room, "[STATUS]", `failed to send message`);
+	// 	}
+	// });
+
+	addMessage(name, "System", `Welcome to ${name}`);
 
 	var node = roomTemplate.content.cloneNode(true);
 	var room = node.querySelector(".room");
@@ -53,13 +124,16 @@ function addRoom(name) {
 	roomListDiv.appendChild(node);
 
 	STATE[name] = [];
-	changeRoom(name);
+	if (change) {
+		changeRoom(name);
+	}
 	return true;
 }
 
 // Change the current room to `name`, restoring its messages.
 function changeRoom(name) {
 	if(STATE.room == name) return;
+	let failed = false;
 
 	var newRoom = roomListDiv.querySelector(`.room[data-name='${name}']`);
 	var oldRoom = roomListDiv.querySelector(`.room[data-name='${STATE.room}']`);
@@ -74,12 +148,34 @@ function changeRoom(name) {
 			messagesDiv.removeChild(msg);
 		});
 
-	STATE[name].forEach((data) => addMessage(name, data.username, data.message));
+	// STATE[name].forEach((data) => addMessage(name, data.username, data.message, data.color));
+
+	fetch("/messages", {
+		method: "POST",
+		body: new URLSearchParams({
+			room_name: name,
+		}),
+	})
+	.then((response) => {
+		if(response.ok) {
+			return response.json();
+		} else {
+			addMessage(STATE.room, "[STATUS]", `failed to fetch messages`);
+			failed = true;
+			return response.json();
+		}
+	})
+	.then((data) => {
+		for (let i = 0; i < data.length; i++) {
+			addMessage(name, data[i].username, data[i].message, data[i].color)
+		}
+		// addMessage(STATE.room, "[STATUS]", `${JSON.stringify(data)}`);
+	});
 }
 
 // Add `message` from `username` to `room`. If `push`, then actually store the
 // message. If the current room is `room`, render the message.
-function addMessage(room, username, message, push = false) {
+function addMessage(room, username, message, color_ = "hash", push = false) {
 	if(push) {
 		STATE[room].push({
 			username,
@@ -87,10 +183,23 @@ function addMessage(room, username, message, push = false) {
 		});
 	}
 
+	
+
 if(STATE.room == room && !muted.includes(username)) {
 		var node = messageTemplate.content.cloneNode(true);
 		node.querySelector(".message .username").textContent = `${username}:`;
-		node.querySelector(".message .username").style.color = hashColor(username);
+
+		if (color_ === "hash") {
+			if (username == undefined) {
+				addMessage(STATE.room, "[ERROR]", "username is undefined")
+				node.querySelector(".message .username").style.color = "#000000";
+			} else {
+				node.querySelector(".message .username").style.color = hashColor(username);
+			}
+		} else {
+			node.querySelector(".message .username").style.color = color_;
+		}
+			
 		node.querySelector(".message .text").textContent = message;
 		node.querySelector(".message .id").textContent = id;
 		node.querySelector(".message").title = id;
@@ -108,8 +217,11 @@ function subscribe(uri) {
 
 		events.addEventListener("message", (ev) => {
 			const msg = JSON.parse(ev.data);
-			if(!"message" in msg || !"room" in msg || !"username" in msg) return;
-			addMessage(msg.room, msg.username, msg.message, true);
+			if(!"message" in msg || !"room" in msg || !"username" in msg || !"color" in msg) return;
+			if (debug) {
+				addMessage(STATE.room, "[DEBUG]", `color is ${msg.color}`);
+			}
+			addMessage(msg.room, msg.username, msg.message, msg.color);
 		});
 
 		events.addEventListener("open", () => {
@@ -143,7 +255,7 @@ function init() {
 	// Initialize some rooms.
 	addRoom("lobby");
 	changeRoom("lobby");
-	addMessage("lobby", "System", "Welcome to the chat!", true);
+	addMessage("lobby", "System", "Welcome to the chat!", push=true);
 
 
 	// Set up the form handler.
@@ -165,12 +277,12 @@ function init() {
 			})
 			.then((response) => {
 				if(response.ok) {
-					if(debug) {
-						addMessage(STATE.room, "[STATUS]", `old_username: ${old_username}, new_username: ${new_username}`);
+					if (debug) {
+							addMessage(STATE.room, "[DEBUG]", `old_username: ${old_username}, new_username: ${new_username}`);
 					}
 					old_username = username;
-					if(debug) {
-						addMessage(STATE.room, "[STATUS]", `registered username ${username}`);
+					if (debug) {
+							addMessage(STATE.room, "[DEBUG]", `registered username ${username}`);
 					}
 				} else {
 					addMessage(STATE.room, "[STATUS]", `failed to register username ${username}`);
@@ -235,6 +347,7 @@ function init() {
 				addMessage(STATE.room, "[STATUS]", `/remove [ID] - removes the message with the id specified (find the ID by hovering over the message)`);
 				addMessage(STATE.room, "[STATUS]", `/remove_range [LOWER] [UPPER] - removes the messages within the range of ids specified [UPPER, LOWER]`);
 				addMessage(STATE.room, "[STATUS]", `/debug - enables debug mode`);
+				addMessage(STATE.room, "[STATUS]", `/color [COLOR] - accepts any valid css color up to 24 chars long, default is hash`);
 				addMessage(STATE.room, "[STATUS]", `/online - shows all online users`);
 				addMessage(STATE.room, "[STATUS]", `/mute [USERNAME] - mutes the selected user`);
 				addMessage(STATE.room, "[STATUS]", `/unmute [USERNAME] - unmutes selected user`);
@@ -247,34 +360,60 @@ function init() {
 					if(node[i].textContent >= args[2]) {
 						node[i].parentNode.remove();
 						if (debug) {
-							addMessage(STATE.room, "[STATUS]", `removing a message`);
+							addMessage(STATE.room, "[DEBUG]", `removing a message`);
 						}
 					}
 
 					if (!node[i].textContent > args[2]) {
 						if (debug) {
-							addMessage(STATE.room, "[STATUS]", `end of loop`);
+							addMessage(STATE.room, "[DEBUG]", `end of loop`);
 						}
 						break
 					}
 				}
-			} else if(STATE.connected) {
-			fetch("/message", {
-					method: "POST",
-					body: new URLSearchParams({
-						room,
-						username,
-						message
-					}),
-				})
-				.then((response) => {
-					if(response.ok) {
-						messageField.value = "";
-					} else {
-						addMessage(STATE.room, "[STATUS]", `failed to send message`);
+			} else if (message.startsWith("/color")) {
+				let args = message.split(' ');
+
+				color = args[1];
+
+				if (debug) {
+					addMessage(STATE.room, "[DEBUG]", `color has changed to ${color}`)
+				}
+
+				messageField.value = "";
+			} else if (message.startsWith("/clear_debug")) {
+				let node = document.getElementsByClassName('username');
+		
+				for(let i = 0; i < node.length; i++) {
+					if(node[i].textContent === "[DEBUG]") {
+						node[i].parentNode.remove();
+						if (debug) {
+							addMessage(STATE.room, "[DEBUG]", `username is ${node[i].textContent}`)
+						}
 					}
-				});
-		}
+				}
+				messageField.value = "";
+			} else if (message.startsWith("/dedup_rooms")) {
+				dedupRooms();
+				
+				messageField.value = "";
+			} else if(STATE.connected) {
+				fetch("/message", {
+						method: "POST",
+						body: new URLSearchParams({
+							room,
+							username,
+							message,
+							color
+						}),
+					}).then((response) => {
+						if(response.ok) {
+							messageField.value = "";
+						} else {
+							addMessage(STATE.room, "[STATUS]", `failed to send message`);
+						}
+					});
+			}
 	});
 
 	// Set up the new room handler.
@@ -287,11 +426,22 @@ function init() {
 		roomNameField.value = "";
 		if(!addRoom(room)) return;
 
-		addMessage(room, "System", `Welcome to ${room}!`, true);
+	//	addMessage(room, "System", `Welcome to ${room}!`, push=true);
 	});
+
+	refershButton.addEventListener("click", (e) => {
+		e.preventDefault();
+
+		refershRooms();
+		dedupRooms();
+	});
+
+	
 
 	// Subscribe to server-sent events.
 	subscribe("/events");
+	refershRooms();
+	changeRoom("lobby");
 }
 
 init();
